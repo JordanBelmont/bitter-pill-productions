ca04b83189cdcbf833c9e0224273cd5816ad541e
Jordan Belmont <55675483+JordanBelmont@users.noreply.github.com>
2022-10-26 15:32:29 +0100

    Created darkMode.js and added defer script in all .html pages

---
+++ javascript/darkMode.js
@@ -0,0 +1,38 @@
+// Dark Mode Toggle
+
+let swtch = document.querySelector('.check');
+let logo = document.querySelector('.pill-light');
+let logoLight = "icons/logo/bitterpill-light.png";
+let logoDark = "icons/logo/bitterpill-dark.png";
+
+
+let currentTheme = localStorage.getItem("theme");
+if (currentTheme === "dark-theme") {
+   document.body.classList.add("dark-theme");
+   swtch.checked = true;
+   logo.classList.add('pill-dark');
+   logo.src = logoDark;
+}
+
+swtch.addEventListener("click", () => {
+   if (document.body.classList.contains("dark-theme")) {
+      localStorage.setItem('theme', '');
+      document.body.classList.remove('dark-theme');
+      swtch.classList.remove('checked');
+      logo.src = logoLight;
+      logo.classList.remove('pill-dark');
+
+   } else {
+      localStorage.setItem('theme', 'dark-theme');
+      document.body.classList.add('dark-theme');
+      logo.src = logoDark;
+      logo.classList.add('pill-dark');
+   }
+});
+
+
+
+
+
+
+
